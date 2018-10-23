8aa1eccc0b39514c76f1777f7446fa915852e75d
DeAngelo Rieke <DERieke@student.fullsail.edu>
2018-10-23 06:57:05 -0400

    First commit

---
+++ app/src/main/java/com/example/deangelorieke/dnd/ExistingCharacters.java
@@ -0,0 +1,13 @@
+package com.example.deangelorieke.dnd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ExistingCharacters extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_existing_characters);
+    }
+}
