c04484cecd23fe59785c5f1b89301d072603e4ee
DeAngelo Rieke <DERieke@student.fullsail.edu>
2018-10-30 07:22:29 -0400

    Random number generation and modification works correctly for the roll all scenario. This also sets the number and saves it for each skill

--- app/src/main/java/com/example/deangelorieke/dnd/statallocation.java
+++ app/src/main/java/com/example/deangelorieke/dnd/statallocation.java
@@ -5,6 +5,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class statallocation extends AppCompatActivity {
 
@@ -14,11 +18,42 @@ public class statallocation extends AppCompatActivity {
         setContentView(R.layout.activity_statallocation);
 
         Button back = findViewById(R.id.statsBackButton);
+        Button rollAll = findViewById(R.id.statsRollAll);
+        Button rollSelected = findViewById(R.id.statsRollSel);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent ccBack = new Intent(statallocation.this, NewCharacter.class);
                 startActivityForResult(ccBack, 1);
+            }
+        });
+        rollAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random rand1 = new Random();
+                Random rand2 = new Random();
+                Random rand3 = new Random();
+                Random rand4 = new Random();
+                Random rand5 = new Random();
+                Random rand6 = new Random();
+                int str = rand1.nextInt(20) + 1;
+                int dex = rand2.nextInt(20) + 1;
+                int con = rand3.nextInt(20)+1;
+                int intel = rand4.nextInt(20) + 1;
+                int wis = rand5.nextInt(20) + 1;
+                int cha = rand6.nextInt(20) + 1;
+                EditText editText = (EditText)findViewById(R.id.strNum);
+                editText.setText(Integer.toString(str), TextView.BufferType.EDITABLE);
+                EditText editText1 = (EditText)findViewById(R.id.dexNum);
+                editText1.setText(Integer.toString(dex), TextView.BufferType.EDITABLE);
+                EditText editText2 = (EditText)findViewById(R.id.conNum);
+                editText2.setText(Integer.toString(con), TextView.BufferType.EDITABLE);
+                EditText editText3 = (EditText)findViewById(R.id.intNum);
+                editText3.setText(Integer.toString(intel), TextView.BufferType.EDITABLE);
+                EditText editText4 = (EditText)findViewById(R.id.wisNum);
+                editText4.setText(Integer.toString(wis), TextView.BufferType.EDITABLE);
+                EditText editText5 = (EditText)findViewById(R.id.charNum);
+                editText5.setText(Integer.toString(cha), TextView.BufferType.EDITABLE);
             }
         });
     }
