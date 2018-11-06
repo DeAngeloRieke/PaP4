dcecb29e027d78919d8085d3a01496c9c9791138
DeAngelo Rieke <DERieke@student.fullsail.edu>
2018-11-06 07:58:02 -0500

    Shared preferences done more efficiently in the NewCharacter.java class.

--- app/src/main/java/com/example/deangelorieke/dnd/NewCharacter.java
+++ app/src/main/java/com/example/deangelorieke/dnd/NewCharacter.java
@@ -1,6 +1,8 @@
 package com.example.deangelorieke.dnd;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -21,10 +23,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class NewCharacter extends AppCompatActivity {
+    public int count = 0;
+    SharedPreferences prefs;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mToggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        prefs = this.getSharedPreferences(
+                "com.example.deangelorieke.dnd", Context.MODE_PRIVATE);
+        count = prefs.getInt("count",0);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_character);
 
@@ -188,8 +195,13 @@ public class NewCharacter extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                EditText ET = (EditText) findViewById(R.id.nameInput);
-                C.setCName(ET.getText().toString());
+                EditText nameET = (EditText) findViewById(R.id.nameInput);
+                String nameSt = nameET.getText().toString();
+                Spinner lvlET = (Spinner) findViewById(R.id.lvlSpinner);
+                String lvlSt = lvlET.getSelectedItem().toString();
+                Spinner classET = (Spinner) findViewById(R.id.classSpinner);
+                String classST = classET.getSelectedItem().toString();
+                addToSharedPref(nameSt, lvlSt, classST);
                 Intent ccNext = new Intent(NewCharacter.this, statallocation.class);
                 startActivityForResult(ccNext, 1);
             }
@@ -244,68 +256,14 @@ public class NewCharacter extends AppCompatActivity {
         Intent dmIntent = new Intent(this,DMMode.class);
         startActivity(dmIntent);
     }
+    public void addToSharedPref(String name, String lvl, String Class)
+    {
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("name" + count, name);
+        editor.putString("lvl" + count, lvl);
+        editor.putString("class" + count, Class);
+        count++;
+        editor.putInt("count", count);
+        editor.apply();
+    }
 }
-
-//Backgrounds:
-//      Acolyte          	PHB	Insight (WIS), religion (INT)	Any x2	None
-//      Anthropologist	        ToA	Insight (WIS), religion (INT)	Any x2	None
-//      Archaeologist	        ToA	History (INT), survival (WIS)	Any x1	Choose 1: cartographer's tools, navigator's tools
-//      Black Fist            Double Agent	CoS (AL)	Deception (CHA), insight (WIS)	None	Disguise kit, and choose 1: artisan's tools x1, gaming set x1
-//      Caravan Specialist      	EE (AL)	Animal handling (WIS), survival (WIS)	Any x1	Vehicles (land)
-//      Charlatan	         PHB	Deception (CHA), sleight of hand (DEX)	None	Disguise kit, forgery kit
-//      City Watch	          SCAG	Athletics (STR), insight (WIS)	Any x2	None
-//      Clan Crafter	       SCAG	History (INT), insight (WIS)	Any x1	Artisan's tools x1
-//      Cloistered Scholar	       SCAG	History (INT), and choose 1: arcana (INT), nature (INT), religion (INT)	Any x2	None
-//      Cormanthor Refugee	        RoD (AL)	Nature (INT), survival (WIS)	Elvish	Artisan's tools x1
-//      Courtier	       SCAG	Insight (WIS), persuasion (CHA)	Any x2	None
-//      Criminal	         PHB	Deception (CHA), stealth (DEX)	None	Gaming set x1, thieves' tools
-//      Dissenter	         PS:A	Special (See page)	Special (See page)	Special (See page)
-//      Dragon Casualty	          CoS (AL)	Intimidation (CHA), survival (WIS)	Draconic	Special (See page)
-//      Earthspur Miner      	EE (AL)	Athletics (STR), survival (WIS)	Dwarvish, Undercommon	None
-//      Entertainer         	PHB	Acrobatics (DEX), performance (CHA)	None	Disguise kit, musical instrument x1
-//      Faction Agent	     SCAG	Insight (WIS), and choose 1: special (See page)	Any x2	None
-//      Far Traveler	       SCAG	Insight (WIS), perception (WIS)	Any x1	Choose 1: musical instrument x1, gaming set x1
-//      Folk Hero	              PHB	Animal handling (WIS), survival (WIS)	None	Artisan's tools x1, vehicles (land)
-//      Gate Urchin	               RoD (AL)	Deception (CHA), sleight of hand (DEX)	None	Musical instrument x1, thieves' tools
-//      Gladiator	               PHB	Acrobatics (DEX), performance (CHA)	None	Disguise kit, unusual weapon x1
-//      Guild Artisan	            PHB	Insight (WIS), persuasion (CHA)	Any x1	Artisan's tools x1
-//      Guild Merchant	            PHB	Insight (WIS), persuasion (CHA)	Any x1	Navigator's tools
-//      Harborfolk	                EE (AL)	Athletics (STR), sleight of hand (DEX)	None	Gaming set x1, vehicles (water)
-//      Haunted One                   	CoS	Choose 2: Arcana (INT), investigation (INT), religion (INT), survival (WIS)	Exotic x1	None
-//      Hermit	                   PHB	Medicine (WIS), religion (INT)	Any x1	Herbalism kit
-//      Hillsfar Merchant               	RoD (AL)	Insight (WIS), persuasion (CHA)	None	Vehicles (land), vehicles (water)
-//      Hillsfar Smuggler	         RoD (AL)	Perception (WIS), stealth (DEX)	Any x1	Forgery kit
-//      House Agent	                  WGE	Investigation (INT), persuasion (CHA)	None	Special (See page)
-//      Inheritor	               SCAG	Survival (WIS), and choose 1: arcana (INT), history (INT), religion (INT)	None	Gaming set x1, musical instrument x1
-//      Initiate	             PS:A	Athletics (STR), intimidation (CHA)	None	Gaming set x1, vehicles (land)
-//      Inquisitor	                PS:In	Investigation (INT), religion (INT)	None	Artisan's tools x1, thieves' tools
-//      Investigator              	SCAG	Insight (WIS), investigation (INT)	Any x2	None
-//      Iron Route Bandit                 	CoS (AL)	Animal handling (WIS), stealth (DEX)	None	Gaming set x1, vehicles (land)
-//      Knight                      	PHB	History (INT), persuasion (CHA)	Any x1	Gaming set x1
-//      Knight of the Order	                 SCAG	Persuasion (CHA), and choose 1: special (See page)	Any x1	Choose 1: gaming set x1, musical instrument x1
-//      Mercenary Veteran	              SCAG	Athletics (STR), persuasion (CHA)	None	Gaming set x1, vehicles (land)
-//      Mulmaster Aristocrat	               EE (AL)	Deception (CHA), performance (CHA)	None	Artisan's tools x1, musical instrument x1
-//      Noble	                     PHB	History (INT), persuasion (CHA)	Any x1	Gaming set x1
-//      Outlander	                PHB	Athletics (STR), survival (WIS)	Any x1	Musical instrument x1
-//      Phlan Insurgent	                 CoS (AL)	Stealth (DEX), survival (WIS)	None	Artisan's tools x1, vehicles (land)
-//      Phlan Refugee	             EE (AL)	Athletics (STR), insight (WIS)	Any x1	Artisan's tools x1
-//      Pirate	                     PHB	Athletics (STR), perception (WIS)	None	Navigator's tools, vehicles (water)
-//      Sage	                     PHB	Arcana (INT), history (INT)	Any x2	None
-//      Sailor	                     PHB	Athletics (STR), perception (WIS)	None	Navigator's tools, vehicles (water)
-//      Secret Identity         	RoD (AL)	Deception (CHA), stealth (DEX)	None	Disguise kit, forgery kit
-//      Shade Fanatic	             RoD (AL)	Deception (CHA), intimidation (CHA)	Netherese	Forgery kit
-//      Soldier                  	PHB	Athletics (STR), intimidation (CHA)	None	Gaming set x1, vehicles (land)
-//      Spy                        	PHB	Deception (CHA), stealth (DEX)	None	Gaming set x1, thieves' tools
-//      Stojanow Prisoner	                CoS (AL)	Deception (CHA), perception (WIS)	None	Gaming set x1, thieves' tools
-//      Ticklebelly Nomad	                      CoS (AL)	Animal handling (WIS), nature (INT)	Giant	Herbalism kit
-//      Trade Sheriff	                        RoD (AL)	Investigation (INT), persuasion (CHA)	Elvish	Thieves' tools
-//      Urban Bounty Hunter	                 SCAG	Choose 2: deception (CHA), insight (WIS), persuasion (CHA), stealth (DEX)	None	Choose 2: gaming set x1, musical instrument x1, thieves' tools
-//      Urchin	                          PHB	Sleight of hand (DEX), stealth (DEX)	None	Disguise kit, thieves' tools
-//      Uthgardt Tribe Member                	SCAG	Athletics (STR), survival (WIS)	Any x1	Choose 1: artisan's tools x1, musical instrument x1
-//      Vizier                         	PS:A	History (INT), religion (INT)	None	Artisan's tools x1, musical instrument x1
-//      Waterdhavian Noble                  	SCAG	History (INT), persuasion (CHA)	Any x1	Choose 1: gaming set x1, musical instrument x1
-
-//Alignment:
-//Chaotic good evil, neutral; Lawful good, evil, neutral; Neutral good, true neutral, neutral evil;
-
-//Level 1-20;
